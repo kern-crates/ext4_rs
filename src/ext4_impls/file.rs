15849549d2050dd4fb5428ce05117d8f75a253d5
röb <57842588+y5@users.noreply.github.com>
2026-01-26 17:28:03 +0100

    fix: Set `start_bgid` to `0` for very small filesystems

--- src/ext4_impls/file.rs
+++ src/ext4_impls/file.rs
@@ -315,7 +315,8 @@ impl Ext4 {
         let mut new_blocks = 0;
 
         // Start bgid for block allocation
-        let mut start_bgid = 1;
+        let block_group_count = self.super_block.block_group_count();
+        let mut start_bgid = if block_group_count > 1 { 1 } else { 0 };
 
         // Pre-allocate blocks if needed
         let blocks_to_allocate = if iblk_idx >= ifile_blocks as usize {
